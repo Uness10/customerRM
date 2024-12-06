fcb0b5f84d60253a40f9cbf6d662a97cf33ae105
AbdeljalilOtman <145472014+AbdeljalilOtman@users.noreply.github.com>
2024-12-06 14:10:23 +0100

    linking and creating the mysql data base to test the login functionality of the Admin

--- src/main/java/com/crm/customerRM/repositories/AdminRepository.java
+++ src/main/java/com/crm/customerRM/repositories/AdminRepository.java
@@ -1,9 +1,10 @@
 package com.crm.customerRM.repositories;
 
 import java.util.Optional;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.crm.customerRM.entities.Admin;
 
-public interface AdminRepository {
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByUsername(String username);
 }
